625c4b2ca996dfb11ab28a0eafcd722261531674
William(addicted2crypto) <addicted2krypto@gmail.com>
2025-08-01 15:53:50 -0700

    centered finally

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import { Geist, Geist_Mono } from "next/font/google";
-
+import "./globals.css"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ import { Header } from './components/Header';
 
 export default function HomePage() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen min-w-4xl">
-      <div className="max-w-md flex flex-col items-center gap-8">
-       
+    <main className="min-h-screen h-full">
+      <div className="container px-4 py-16 ">
+       <div className='text-center mb-6 mt-10'>
         <Header />
         <Image
           src="/avax-logo.png"
@@ -18,10 +18,13 @@ export default function HomePage() {
           className="rounded-sm shadow-lg w-32 sm:w-40"
           priority
         />
+        </div>
         <h1 className='text-center container'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti blanditiis unde aut ratione repudiandae, harum nihil consectetur. Ipsam ex fugit quod ab culpa nam! Harum quod perferendis voluptate nam est.</h1>
         <ContractInput />
       </div>
+      <div className='pt-10 pb-16'>
       <Footer />
+      </div>
     </main>
   )
 }
