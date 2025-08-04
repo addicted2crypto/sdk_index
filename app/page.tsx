5fd1dfec8d3cc71b3d7e24cb0a684d1ea902d869
William(addicted2crypto) <addicted2krypto@gmail.com>
2025-08-03 22:05:03 -0700

    image addition to fe

--- app/page.tsx
+++ app/page.tsx
@@ -3,28 +3,43 @@ import Image from "next/image";
 import Footer from './components/Footer';
 import ContractInput from './components/ContractInput';
 import { Header } from './components/Header';
+import { Link } from 'lucide-react';
 
 export default function HomePage() {
   return (
-    <main className="min-h-screen grid grid-rows-[20px_1fr_20px] items-center jutstify-items-center p-8 pb-20 sm:p-20">
+    <main className="min-h-screen items-center jutstify-items-center p-8 pb-20">
     
-      <div className="container px-4 py-16 flex items-center justify-between flex-col">
+      <header className="min-h-screen sticky top-0 z-50">
+         <Link href="/" className="flex itmes-center gap-2"> 
+        
+         
+         </Link>
          <Header />
-       <div className='container items-center text-center gap-4 mb-8'>
-       
-        <Image
+        </header>
+       <div className='cointainer mx-auto px-4 py-4'>
+     
+        <div className='flex items-center justify-center'>
+          <span>
+          <h1 className="text-5xl font-bold">AVAX Token Analyze</h1>
+          </span>
+
+        </div>
+        
+   
+         <Image
         aria-hidden="true"
           src="/avax-logo.png"
           alt="AVAX Logo"
-          width={330}
+          width={220}
           height={330}
-          className="mx-auto"
+          className="justify-center items-center"
           priority
         />
+        
         </div>
-        <h1 className='text-center container'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti blanditiis unde aut ratione repudiandae, harum nihil consectetur. Ipsam ex fugit quod ab culpa nam! Harum quod perferendis voluptate nam est.</h1>
+        <h1 className='text-center'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti blanditiis unde aut ratione repudiandae, harum nihil consectetur. Ipsam ex fugit quod ab culpa nam! Harum quod perferendis voluptate nam est.</h1>
         <ContractInput />
-      </div>
+      
       <div className='pt-10 pb-16'>
       <Footer />
       </div>
